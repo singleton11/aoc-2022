2365391aeb7a36a0937d19056c479cafe2330c9b
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2022-12-05 22:26:37 +0100

    Day 5

---
+++ day5/Task1.kt
@@ -0,0 +1,51 @@
+import java.util.Stack
+
+fun main() {
+
+    val stacks = arrayOfNulls<Stack<Char>>(9)
+
+    for (i in stacks.indices) {
+        stacks[i] = Stack()
+    }
+
+    var crates = true
+
+    readResourceAsText("input.txt").lines().forEach {
+        if (it.length > 1 && it[1] == '1') {
+            crates = !crates
+            for (stack in stacks) {
+                stack!!.reverse()
+            }
+        } else {
+            if (crates) {
+                it.forEachIndexed { index, c ->
+                    if (c != ' ' && c != '[' && c != ']') {
+                        stacks[(index - 1) / 4]!!.push(c)
+                    }
+                }
+            } else {
+                // parse moves
+                "move (\\d+) from (\\d+) to (\\d+)".toRegex().findAll(it).firstOrNull()?.let { matchResult ->
+                    if (matchResult.groupValues.isNotEmpty()) {
+                        val (_, count, from, to) = matchResult.groupValues
+                        repeat(count.toInt()) {
+                            val value = stacks[from.toInt() - 1]!!.pop()
+                            stacks[to.toInt() - 1]!!.push(value)
+                        }
+                        println("move $count from $from to $to")
+                    }
+                }
+
+            }
+        }
+    }
+
+    val result = buildString {
+        for (stack in stacks) {
+            append(stack!!.peek())
+        }
+    }
+
+    println(result)
+
+}
